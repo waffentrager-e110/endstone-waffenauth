ab461ba310d8255787a81622a27bbff4a59ede83
waffentrager-e110 <piskarevleonard@gmail.com>
2026-04-18 11:38:01 +0700

    Update listener.py

--- src/waffenauth/listener.py
+++ src/waffenauth/listener.py
@@ -6,23 +6,22 @@ if TYPE_CHECKING:
     from endstone_waffenauth import WaffenAuth
 
 class WaffenAuthListener:
-    def __init__(self, plugin: "WaffenAuth") -> None:
-        self._plugin = plugin
+    def __init__(self, plugin: "WaffenAuth"):
+        self.plugin = plugin
 
     @event_handler
-    def on_player_move(self, event: PlayerMoveEvent) -> None:
+    def on_player_move(self, event: PlayerMoveEvent):
         player = event.player
-        if player.name not in self._plugin.auth_players:
+        if player.name not in self.plugin.authorized:
             event.cancel()
-            player.send_message(self._plugin.get_message("move_blocked"))
+            player.send_message(self.plugin.get_message("move_blocked"))
 
     @event_handler
-    def on_player_command(self, event: PlayerCommandPreprocessEvent) -> None:
+    def on_player_command(self, event: PlayerCommandPreprocessEvent):
         player = event.player
-        if player.name in self._plugin.auth_players:
+        if player.name in self.plugin.authorized:
             return
-        
         cmd = event.message.split()[0].lower()
         if cmd not in ["/register", "/login"]:
             event.cancel()
-            player.send_message(self._plugin.get_message("command_blocked"))
+            player.send_message(self.plugin.get_message("command_blocked"))
